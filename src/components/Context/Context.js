e6f94fb4763d2fdf8df874c459584d4076c34591
Shamim Sarkar <shamimibas@gmail.com>
2022-11-08 23:35:43 +0600

    Installed firebase and created function createuser, loginuser, updateUser

--- src/components/Context/Context.js
+++ src/components/Context/Context.js
@@ -1,9 +1,27 @@
 import React, { createContext, useState } from 'react';
+import {createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, updateProfile} from 'firebase/auth';
+import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext();
+const auth = getAuth(app);
 
 const Context = ({children}) => {
     const [modal, setModal] = useState(false);
+
+    // creating new user with email and password
+    const createNewUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+    
+    // login user with email and password
+    const loginUser = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    //updating user name when rsgister
+    const updateUser = (name) => {
+        return updateProfile(auth.currentUser, {displayName: name});
+    }
 
     const userInfo = {
         modal,
